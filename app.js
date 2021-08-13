f98e7cb3e51edd7dc385dc14e0bd72c94e21f1bb
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-13 11:18:30 +0100

    cleared search and gallery

--- app.js
+++ app.js
@@ -31,8 +31,12 @@ function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+    galleryImg.innerHTML = `
+    <div class="gallery-info"> 
     <p>${photo.photographer}</p>
+    <a href=${photo.src.original}>Download</a>
+    </div>
+    <img src=${photo.src.large}></img>
     `;
     gallery.appendChild(galleryImg);
   });
@@ -46,10 +50,16 @@ async function curatedPhotos() {
 }
 
 async function searchPhotos(query) {
+  clear();
   const data = await fetchApi(
     `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
   );
   generatePictures(data);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
 }
 
 curatedPhotos();
